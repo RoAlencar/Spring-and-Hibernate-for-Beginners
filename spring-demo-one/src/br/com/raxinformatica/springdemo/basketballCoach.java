f5eacd433b031f9a684e5f336ff77ca5dc42fe3f
RoAlencar <roalencarx@gmail.com>
2021-09-08 18:37:27 -0300

    Pratice activity - spring inversion of control

---
+++ spring-demo-one/src/br/com/raxinformatica/springdemo/basketballCoach.java
@@ -0,0 +1,11 @@
+package br.com.raxinformatica.springdemo;
+
+public class basketballCoach implements Coach {
+	
+	@Override
+	public String getDailyWorkout() {
+		return "Try to shoot in the 3-point basket 3 times";
+		
+	}
+
+}
