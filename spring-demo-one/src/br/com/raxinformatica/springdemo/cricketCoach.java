9abc57df02b06c54811b83a2d41c25191a267cf4
RoAlencar <roalencarx@gmail.com>
2021-09-10 16:03:16 -0300

    Injecting values from a properties file

--- spring-demo-one/src/br/com/raxinformatica/springdemo/cricketCoach.java
+++ spring-demo-one/src/br/com/raxinformatica/springdemo/cricketCoach.java
@@ -3,7 +3,12 @@ package br.com.raxinformatica.springdemo;
 public class cricketCoach implements Coach {
 
 	private FortuneService fortuneService;
-	
+
+	// add new fields for emailAddress and team
+	private String emailAddress;
+	private String team;
+
+
 	// create a no-arg constructor
 	public cricketCoach() {
 		System.out.println("CricketCoach: inside no-arg constructor");
@@ -14,7 +19,22 @@ public class cricketCoach implements Coach {
 		System.out.println("CricketCoach: inside setter method");
 		this.fortuneService = fortuneService;
 	}
+	
+	public String getEmailAddress() {
+		return emailAddress;
+	}
 
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+
+	public String getTeam() {
+		return team;
+	}
+
+	public void setTeam(String team) {
+		this.team = team;
+	}
 
 	@Override
 	public String getDailyWorkout() {
